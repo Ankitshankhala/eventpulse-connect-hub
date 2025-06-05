63be9d60d95928bca95f2c2a98534d506cef7569
Ankit Shankhala <ankitshankhala2112@gmail.com>
2025-06-05 14:39:10 +0530

    I have Changes The Color of button and Made changes in Readme File.

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -20,7 +20,7 @@ export const Footer = () => {
         </div>
         
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 EventPulse. All rights reserved.</p>
+          <p>&copy; 2025 EventPulse. All rights reserved.</p>
         </div>
       </div>
     </footer>
